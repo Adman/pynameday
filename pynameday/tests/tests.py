e20b5c247c480ea2a821072ddfba7827af7e662e
Adrian Matejov <a.matejov@centrum.sk>
2015-06-22 12:39:12 +0200

    initial 0.0.1 version
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

---
+++ pynameday/tests/tests.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from pynameday.slovakia import Slovakia
+from pynameday.czechrepublic import CzechRepublic
+
+
+class NamedayMixinTest(object):
+    MONTHS = 12
+    DAYS = (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    cal = None
+
+    def test_months(self):
+        self.assertEqual(len(self.cal), self.MONTHS)
+
+    def test_days(self):
+        for i, j in enumerate(self.cal):
+            self.assertEqual(len(j), self.DAYS[i])
+
+
+class SlovakiaTest(TestCase, NamedayMixinTest):
+    """Slovakian nameday test"""
+    def setUp(self):
+        self.cal = Slovakia.NAMEDAYS
+
+
+class CzechRepublicTest(TestCase, NamedayMixinTest):
+    """Czech nameday test"""
+    def setUp(self):
+        self.cal = CzechRepublic.NAMEDAYS
